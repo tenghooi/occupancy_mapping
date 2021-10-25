ac9054d8506a1500183acca85d243554164a1a1a
tenghooi <chantenghooi@gmail.com>
2021-10-26 02:20:10 +0800

    updateRayCastingHeader

--- include/raycasting.h
+++ include/raycasting.h
@@ -1,11 +1,27 @@
 #ifndef _RAYCASTING_H_
 #define _RAYCASTING_H_
 
-#include <Eigen/Eigen>
+#include <eigen3/Eigen/Eigen>
 #include <vector>
 
-void RayCasting2D()
+void RayCasting2D(const Eigen::Vector2d& ray_origin, const Eigen::Vector2d& ray_end,
+                  const int dummy)
 {
+    // Initialization Step
+    Eigen::Vector2i current_voxel(std::floor(ray_origin.x()), std::floor(ray_origin.y()));
+    Eigen::Vector2i end_voxel(std::floor(ray_end.x()), std::floor(ray_end.y()));
+    Eigen::Vector2d ray_direction {ray_end - ray_origin};
+
+    int stepX {(ray_direction.x() >= 0) ? 1 : -1};
+    int stepY {(ray_direction.y() >= 0) ? 1 : -1};
+    
+    double tMaxX;
+    double tMaxY;
+
+    double tDeltaX;
+    double tDeltaY;
+    
+    // Traversal Step
     for(;;)
     {
         if(tMaxX < tMaxY)
