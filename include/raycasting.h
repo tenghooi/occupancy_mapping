799df9ce80073abc9e5d0a01ff0d854cb93aa9d9
TengHooi <chantenghooi@gmail.com>
2021-10-27 19:24:12 +0800

    updateRayCasting2D

--- include/raycasting.h
+++ include/raycasting.h
@@ -4,41 +4,61 @@
 #include <eigen3/Eigen/Eigen>
 #include <vector>
 
-/*  RayCasting2D and RayCasting3D are based on Amanatides & Woo ray tracing algorithm.
+/*  
+    RayCasting2D and RayCasting3D are based on Amanatides & Woo ray tracing algorithm.
     Their paper "A Fast Voxel Traversal Algorithm for Ray Tracing" can be found here: 
     <https://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.42.3443&rep=rep1&type=pdf>
 
-    Ray equation:  r = u + t*v, where u is the ray origin.
-
+    This algorithm uses a parameterized line equation for ray tracing.
+    Ray equation:  r = r0 + t*v, where r0 is the ray origin, v is difference between ray end and ray origin.
+    
+    E.g: X0 = [0.5 0.5], X1 = [2.5 0]
+         Then, v = [2 -0.5] 
+         r = [0.5 0.5] + t * [2 -0.5]
 */
 
 void RayCasting2D(const Eigen::Vector2d& ray_origin, const Eigen::Vector2d& ray_end,
-                  const int dummy)
+                  std::vector<Eigen::Vector3i>& output_voxels)
 {
 // Initialization Step
-    Eigen::Vector2i current_voxel(std::floor(ray_origin.x()), std::floor(ray_origin.y()));
+    
     Eigen::Vector2i end_voxel(std::floor(ray_end.x()), std::floor(ray_end.y()));
-    Eigen::Vector2d ray_direction {ray_end - ray_origin};
+    Eigen::Vector2d ray_direction = ray_end - ray_origin;
 
-    int x = current_voxel.x();
-    int y = current_voxel.y();
+    int current_voxel_x = std::floor(ray_origin.x());
+    int current_voxel_y = std::floor(ray_origin.y());
 
-    int stepX {(ray_direction.x() >= 0) ? 1 : -1};
-    int stepY {(ray_direction.y() >= 0) ? 1 : -1};
+    double dx = ray_direction.x();
+    double dy = ray_direction.y();
+
+    // step* indicates wheter X or Y are incremented or decremented as the ray crosses voxel boundaries.
+    int stepX {(dx == 0) ? 0 : (dx > 0) ? 1 : -1};
+    int stepY {(dy == 0) ? 0 : (dy > 0) ? 1 : -1};
     
     // t breaks down the ray into intervals of t such that each interval spans one voxel.
     // t is the value which ray first crosses the vertical(horizontal) boundary inside the grid.
-    // tMax* >= 0 
-    // r = x + tMaxX * dx, where r is the first crossed vertical boundary.
-    // Hence, tMaxX = (r-x) / dx
-    double tMaxX {(x + stepX) / ray_direction.x()};
-    double tMaxY {(y + stepY) / ray_direction.y()};
+    // t is incremental from initial t where ray crossed the first boundary to 1 where 
+    // it reaches ray end.
+    //
+    // tMax* >= 0 and tMax* <= 1. When tMax* > 1, it exceeds the ray length.
+    // r = x0 + tMaxX * dx, where r is the x of the first crossed vertical boundary.
+    // Hence, tMaxX = (r - x0) / dx       OR
+    //        tMaxX = (current_voxel_x + voxel_width - x0) / dx
+    // If dx = 0, that means ray will never travel horizontally. Set tMaxX to be large number(100).
+    
+    double tMaxX {(dx != 0) ? (current_voxel_x + stepX - ray_origin.x()) / dx : 100};
+    double tMaxY {(dy != 0) ? (current_voxel_y + stepY - ray_origin.y()) / dy : 100};
 
     // tDeltaX indicates how far along the ray we must move (in units of t) for the horizontal 
     // component of such a movement to equal the width of a voxel.
-    // In other words, to move r 
-    double tDeltaX {stepX / ray_direction.x()};
-    double tDeltaY {stepY / ray_direction.y()};
+    // In other words, divide the width of a voxel by dX or dY of the ray.
+    // E.g. Voxel size = 1; dX = 5.0;
+    //      Then, tDeltaX = 1 / 5;
+    //
+    // tDelta* must be positive like tMax*, i.e. tDelta* >= 0 and <= 1.
+
+    double tDeltaX {(dx != 0) ? stepX / dx : 100};
+    double tDeltaY {(dy != 0) ? stepY / dy : 100};
     
 // Traversal Step
     for(;;)
